4165b511be7c24e60b82b72b1bdb7d2ec3aa5a3c
denvyworking <denis04012006sobolev@gmail.com>
2025-08-21 21:15:54 +0300

    Добавили базу данных с миграциями без SQL запросов

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/Knetic/govaluate"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
+func initDB() {
+	dsn := "host=localhost user=postgres password=yourpassword dname=postgres port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+	if err := db.AutoMigrate(&Calculation{}); err != nil {
+		log.Fatalf("Could not migrate: %v", err)
+	}
+}
+
 type Calculation struct {
-	ID         string `json:"id"`
+	ID         string `gorm:"primaryKey" json:"id"`
 	Expression string `json:"expression"`
 	Result     string `json:"result"`
 }
